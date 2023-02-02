d642416bce5d46f9a38be56645457bf3bb31fc61
Chloe Wahl-Dassule <cwahldassule c:; cd 'c:\Users\Chloe\OneDrive\Documents\CSC 120 files\CSC120-git-demo'; & 'C:\Users\Chloe\AppData\Local\Programs\Python\Python310\python.exe' 'c:\Users\Chloe\.vscode\extensions\ms-python.python-2022.20.2\pythonFiles\lib\python\debugpy\adapter/../..\debugpy\launcher' '61049' '--' 'extension-output-vscode.git-#1-Git' @smith.edu>
2023-02-02 12:00:51 -0500

    first attempt

--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,33 @@
+from computer import *
 class ResaleShop:
 
     # What attributes will it need?
+    inventory = []
+    profits = 0.0
 
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def __init__(self, inventory, profits):
+        self.inventory = inventory
+        self.profits = profits
 
-    # What methods will you need?
+    def update_inventory(self, action):
+        if action == 'buying':
+            self.inventory + self
+        if action == 'selling':
+            self.inventory - self
+    
+    def buycomp(self, name, price, OS):
+        pass
+    
+    def refursbish(self, new_price, new_OS):
+        self.updateOS(new_OS)
+        self.updateprice(new_price)
+        self.update_inventory("selling")
+
+
+
+def main():
+    pass
+
+main()
