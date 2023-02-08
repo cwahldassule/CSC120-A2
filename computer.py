586416e4927ca0fa6d44f0553d6cd59d0cc1c22e
Chloe Wahl-Dassule <cwahldassule c:; cd 'c:\Users\Chloe\OneDrive\Documents\CSC 120 files\CSC120-git-demo'; & 'C:\Users\Chloe\AppData\Local\Programs\Python\Python310\python.exe' 'c:\Users\Chloe\.vscode\extensions\ms-python.python-2022.20.2\pythonFiles\lib\python\debugpy\adapter/../..\debugpy\launcher' '61049' '--' 'extension-output-vscode.git-#1-Git' @smith.edu>
2023-02-07 20:57:33 -0500

    fixed main

--- computer.py
+++ computer.py
@@ -1,10 +1,47 @@
-class Computer:
+class computer:
+    name: str
+    description: str
+    processor_type: str
+    hard_drive_capacity: int
+    memory: int
+    operating_system: str
+    year_made: int
+    price: int
+    
+    def __init__(self, name, descrip, pro_type, hard_drive_cap, memory, OpSy, year_made, price) -> None:
+        self.name = name
+        self.description = descrip
+        self.processor_type = pro_type
+        self.hard_drive_capacity = hard_drive_cap
+        self.memory = memory
+        self.operating_system = OpSy
+        self.year_made = year_made
+        self.price = price
+    
+    def updateprice(self, amt) -> bool:
+        self.price = amt
+        return True
+    
+    def updateOS(self, newOS) -> bool:
+        self.operating_system = newOS
+        return True
+    
+    def print_details(self) -> None:
+        print("\nDescription:",self.description)
+        print("Processor:",self.processor_type)
+        print( "Hard Drive:",self.hard_drive_capacity)
+        print("Memory:", self.memory)
+        print("Operating System:", self.operating_system )
+        print("Year Made:",self.year_made)
+        print("Price:", self.price)
 
-    # What attributes will it need?
-
-    # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
-
-    # What methods will you need?
+#def main():
+#    DoeVoid = computer("DoeVoid", "Mac Pro (Late 2013)",
+#                        "3.5 GHc 6-Core Intel Xeon E5", 1024, 64, 
+#                        "macOS Big Sur", 2013, 1500)
+#    DoeVoid.print_details()
+#    DoeVoid.updateprice(1700)
+#    DoeVoid.print_details()
+#    DoeVoid.updateOS("FlipFlop2")
+#    DoeVoid.print_details()
+#main()
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -3,31 +3,65 @@ class ResaleShop:
 
     # What attributes will it need?
     inventory = []
-    profits = 0.0
 
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__(self, inventory, profits):
-        self.inventory = inventory
-        self.profits = profits
+    def __init__(self):
+        self.inventory = []
 
-    def update_inventory(self, action):
-        if action == 'buying':
-            self.inventory + self
-        if action == 'selling':
-            self.inventory - self
+    def buy(self, c):
+        print(f"\nBuying {c.name}...")
+        self.inventory.append(c)
+        print("Done :)")
+
+    def sell(self, c):
+        if c in self.inventory:
+            print(f"\nSelling {c.name}...")
+            self.inventory.remove(c)
+            print("Done :)")
+        else:
+            print("Error! Item not in inventory :()")
+
+    def refurbish(self, c,new_OS):
+        if c in self.inventory:
+            print(f"\nRefurbishing {c.name}...")
+            if c.year_made < 2000:
+                c.updateprice(0)
+                print(f"{c.name} is too old to sell, donation only")
+            elif c.year_made < 2012:
+                c.updateprice(250)
+                print(f"{c.name} is 10+ years old, heavily-discounted")
+            elif c.year_made < 2018:
+                c.updateprice(550)
+                print(f"{c.name} is 4 to 10 years old, discounted")  
+            elif c.year_made < 2018:
+                c.updateprice(1000)
+                print(f"{c.name} is recent")
+            if new_OS is not None:
+                c.updateOS(new_OS)
+            print("Done :)")
+        else:
+            print(f"\n{c.name} is not in inventory :()")            
     
-    def buycomp(self, name, price, OS):
-        pass
-    
-    def refursbish(self, new_price, new_OS):
-        self.updateOS(new_OS)
-        self.updateprice(new_price)
-        self.update_inventory("selling")
-
-
+    def print_store(self) -> None:
+        if self.inventory == []:
+            print("\nNo Inventory :()")
+        else:
+            print("\nInventory:")
+            for i in self.inventory:
+                i.print_details()
 
 def main():
-    pass
+    print('--Welcome to Resale Shop!--\n')
+    DoeVoid = computer("DoeVoid", "Mac Pro (Late 2013)",
+                        "3.5 GHc 6-Core Intel Xeon E5", 1024, 64, 
+                        "macOS Big Sur", 2013, 1500)
+    ResaleShop.buy(ResaleShop, DoeVoid)
+    ResaleShop.print_store(ResaleShop)
+    ResaleShop.refurbish(ResaleShop, DoeVoid,"FlipFlop2")
+    ResaleShop.print_store(ResaleShop)
+    ResaleShop.sell(ResaleShop, DoeVoid)
+    ResaleShop.print_store(ResaleShop)
+
 
 main()
